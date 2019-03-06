ac4666bf69d5167fa7dcf0759ff62ce92fa93aa4
Raí Tamarindo <raitamarindo@gmail.com>
2019-03-06 19:03:54 -0300

    Implemented customer data validation

--- src/middlewares/customer.validator.ts
+++ src/middlewares/customer.validator.ts
@@ -1,9 +1,17 @@
 import { RequestHandler } from 'express';
+import { check } from 'express-validator/check';
 import { userValidator } from './user.validator';
-// import { validationHandlerMiddleware } from './validation-handler.middleware';
+import { validationHandlerMiddleware } from './validation-handler.middleware';
 
-export const customerValidator: RequestHandler[] = userValidator('user')
+export const customerValidator: RequestHandler[] = userValidator('userData')
     .concat([
+        check('userData')
+            .exists()
+            .withMessage('Cutomer user data is required.'),
 
-        // validationHandlerMiddleware,
+        check('shippingRegion')
+            .exists()
+            .withMessage('Customer shipping region is required.'),
+
+        validationHandlerMiddleware,
     ]);
